0cfd75eda92dce87581c7a96d213d7cf00f4cf21
MenzoProject <masonbrookss82@gmail.com>
2026-07-27 23:35:04 +0700

    Add AdminBanksCallback and related keyboard functions

--- src/bot/keyboards/admin/__init__.py
+++ src/bot/keyboards/admin/__init__.py
@@ -5,6 +5,11 @@ from src.bot.keyboards.admin.applications import (
     get_admin_application_card_keyboard,
     get_admin_applications_queue_keyboard,
 )
+from src.bot.keyboards.admin.banks import (
+    AdminBanksCallback,
+    get_admin_banks_list_keyboard,
+    get_admin_settings_keyboard,
+)
 from src.bot.keyboards.admin.main_menu import (
     ADMIN_MENU_BUTTON_TEXTS,
     ADMIN_MENU_PAYMENTS,
@@ -33,16 +38,19 @@ __all__ = [
     "ADMIN_MENU_SETTINGS",
     "ADMIN_MENU_STATISTICS",
     "AdminApplicationsCallback",
+    "AdminBanksCallback",
     "AdminPaymentsCallback",
     "AdminProductsCallback",
     "ProductPhotoSkipCallback",
     "YesNoCallback",
     "get_admin_application_card_keyboard",
     "get_admin_applications_queue_keyboard",
+    "get_admin_banks_list_keyboard",
     "get_admin_main_menu_keyboard",
     "get_admin_payments_list_keyboard",
     "get_admin_product_card_keyboard",
     "get_admin_products_list_keyboard",
+    "get_admin_settings_keyboard",
     "get_photo_step_keyboard",
     "get_yes_no_keyboard",
 ]
